5bdcd00f473c15bf7ae8ef55d030d2ee778dd993
Max <maks.popoff2009@yandex.ru>
2023-04-19 19:56:48 +0500

    9 sprint

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.exception;
 
-import java.io.IOException;
 
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
