8c9d8d092f412aa284dfef58d0c098796b504a06
Max <maks.popoff2009@yandex.ru>
2023-05-31 20:11:49 +0500

    fix: добавлена реалицация работы с БД

--- src/main/java/ru/yandex/practicum/filmorate/conrtollers/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/conrtollers/GenreController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.dao.GenreDao;
 import ru.yandex.practicum.filmorate.model.Genre;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 @Slf4j
